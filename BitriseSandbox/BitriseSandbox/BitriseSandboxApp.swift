6d3da6b59b2d03bade25da5991fc7b425b2933d6
Brian Drelling <brian.drelling@gmail.com>
2021-04-28 15:33:07 -0500

    Added Xcode project and Fastfile

---
+++ BitriseSandbox/BitriseSandbox/BitriseSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseSandboxApp.swift
+//  BitriseSandbox
+//
+//  Created by Brian Drelling on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BitriseSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
